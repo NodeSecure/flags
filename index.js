7214f0475dba934a2ff413683577b3688b25b7c4
Vincent Dhennin <vincent.dhennin@viacesi.fr>
2021-08-10 05:41:16 +0200

    lint

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 // Import Node.js Dependencies
-import { createReadStream } from 'fs';
-import { join } from 'path';
-import { fileURLToPath } from 'url';
-import { readFile } from 'fs/promises';
+import { createReadStream } from "fs";
+import { join } from "path";
+import { fileURLToPath } from "url";
+import { readFile } from "fs/promises";
 
 const __dirname = fileURLToPath(import.meta.url);
 
 const FLAGS = JSON.parse(
   await readFile(
-    new URL('src/manifest.json', import.meta.url)
+    new URL("src/manifest.json", import.meta.url)
   )
 );
 
 // Export src/manifest.json
 export function getManifest() {
-  return FLAGS
+  return FLAGS;
 }
 
 // Return set flags
@@ -25,8 +25,8 @@ export function getFlags() {
 // Read specific flag file
 export function getFlagFile(name) {
   if (!name) {
-    throw new Error("You should provide a flag name")
+    throw new Error("You should provide a flag name");
   }
 
-  return createReadStream(join(__dirname, `../src/flags/${name}.html`))
+  return createReadStream(join(__dirname, `../src/flags/${name}.html`));
 }
