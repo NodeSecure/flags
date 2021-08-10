68b53d35d5d8e230a072acfa039a7f621c9e696b
Vincent Dhennin <vincent.dhennin@viacesi.fr>
2021-08-10 06:33:25 +0200

    simplify generateFlag

--- src/scripts/generateFlags.js
+++ src/scripts/generateFlags.js
@@ -1,55 +1,40 @@
 // Import Node.js Dependencies
-import { join } from 'path'
-import { readdir, readFile, writeFile } from 'fs/promises'
-import { fileURLToPath } from 'url';
+import { join } from "path";
+import { readdir, readFile, writeFile } from "fs/promises";
+import { fileURLToPath } from "url";
 
 // Import Third-party Dependencie
-import TurndownService from 'turndown'
+import TurndownService from "turndown";
 
 const __dirname = fileURLToPath(import.meta.url);
 
-async function * iterateOverFiles(flagsPath) {
-  try {
-    const files = await readdir(flagsPath);
-    for (const filePath of files)
-      yield readFile(join(flagsPath, filePath))
-  } catch (err) {
-    console.error(err)
-  }
-}
-
 async function generateFlagsDocumentation() {
+  const flagsPath = join(__dirname, "../../flags/");
+  const templateDocumentation = await readFile(join(__dirname, "../templateFlagDoc.md"));
 
-  const flagsPath = join(__dirname, `../../flags/`);
-  const templateDocumentation = await readFile(join(__dirname, '../templateFlagDoc.md'))
+  const result = [];
 
-  let result = []
-  
-  for await (const file of iterateOverFiles(flagsPath)) {
-    result.push(file)
+  const files = await readdir(flagsPath);
+
+  for (const file of files) {
+    result.push(await readFile(join(flagsPath, file)));
   }
 
-  const turndownService = new TurndownService()
+  const turndownService = new TurndownService();
 
-  turndownService.addRule('summary', {
-    filter: 'summary',
-    replacement: (content) => {
-      return `<summary>${content}</summary>`
-    }
+  turndownService.addRule("summary", {
+    filter: "summary",
+    replacement: (content) => `<summary>${content}</summary>`
   });
 
-  turndownService.addRule('details', {
-    filter: 'details',
-    replacement: (content) => {
-      return `<details>${content}</details>`
-    }
+  turndownService.addRule("details", {
+    filter: "details",
+    replacement: (content) => `<details>${content}</details>`
   });
 
-  let mdFile = turndownService.turndown(result.join("").toString())
+  const mdFile = turndownService.turndown(result.join("").toString());
 
-  writeFile("FLAGS.md", templateDocumentation.toString().concat(mdFile), (err) => {
-    if (err) console.error(err)
-  })
+  writeFile("FLAGS.md", templateDocumentation.toString().concat(mdFile));
 }
 
-await generateFlagsDocumentation()
+await generateFlagsDocumentation();
