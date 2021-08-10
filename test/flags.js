2621756c45afeaf344a0721083de29fb7cee5b96
Vincent Dhennin <vincent.dhennin@viacesi.fr>
2021-08-10 22:07:24 +0200

    fix tests

--- test/flags.js
+++ test/flags.js
@@ -3,24 +3,24 @@ import { getFlags, getManifest, getFlagFile } from "../index.js";
 
 // get flag
 test("get set of flags", (tape) => {
-  const flags = getFlags()
+  const flags = getFlags();
 
-  tape.equal(typeof flags, 'object')
-  tape.doesNotEqual(flags.size, 0)
-  tape.end()
-})
+  tape.equal(typeof flags, "object");
+  tape.doesNotEqual(flags.size, 0);
+  tape.end();
+});
 
 // get manifest
 test("get manifest", (tape) => {
-  const manifest = getManifest()
-  tape.equal(typeof manifest, 'object')
-  tape.end()
-})
+  const manifest = getManifest();
+  tape.equal(typeof manifest, "object");
+  tape.end();
+});
 
 // get flag file
-test("get flag file", async (tape) => {
-  const flagFile = await getFlagFile('hasBannedFile')
+test("get flag file", async(tape) => {
+  const flagFile = await getFlagFile("hasBannedFile");
 
-  tape.equal(typeof flagFile, 'string')
-  tape.end()
-})
+  tape.equal(typeof flagFile, "object");
+  tape.end();
+});
